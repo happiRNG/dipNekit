ea8efd9f9d1d57cbdd9c15cfe42e11be125d99fe
happiRNG <happirng@gmail.com>
2022-03-17 20:52:41 +0500

    asd

--- pages/main/information_on_emergency_situations/information_on_emergency_situations.php
+++ pages/main/information_on_emergency_situations/information_on_emergency_situations.php
@@ -9,27 +9,17 @@
 <p style="text-indent:20px;" align="justify"> <a href=http://www.mchs.gov.ru>  Сайт МЧС России</a>
 
 
-
-<table align=center><td><img src="/sources/main/information_on_emergency_situations/g.jpg"></td></table>
-
-
-<table align=center><td><img src="/sources/main/information_on_emergency_situationss/g1.jpg"></td></table>
-
-<table align=center><td><img src="/sources/main/information_on_emergency_situations/g2.jpg"></td></table>
-
-
-<table align=center><td><img src="/sources/main/information_on_emergency_situations/g3.jpg"></td></table>
-
-<table align=center><td><img src="/sources/main/information_on_emergency_situations/g4.jpg"></td></table>
-
-
-<table align=center><td><img src="/sources/main/information_on_emergency_situations/g5.jpg"></td></table>
-
-<table align=center><td><img src="/sources/main/information_on_emergency_situations/g6.jpg"></td></table>
-
-
-<table align=center><td><img src="/sources/main/information_on_emergency_situations/g7.jpg"></td></table>
-  </font>
+    <div class="column">
+<img src="/sources/main/information_on_emergency_situations/g.jpg">
+<img src="/sources/main/information_on_emergency_situations/g1.jpg">
+<img src="/sources/main/information_on_emergency_situations/g2.jpg">
+<img src="/sources/main/information_on_emergency_situations/g3.jpg">
+<img src="/sources/main/information_on_emergency_situations/g4.jpg">
+<img src="/sources/main/information_on_emergency_situations/g5.jpg">
+<img src="/sources/main/information_on_emergency_situations/g6.jpg">
+<img src="/sources/main/information_on_emergency_situations/g7.jpg">
+</div> 
+</font>
 	
 		
 		<ul class="mlist">
