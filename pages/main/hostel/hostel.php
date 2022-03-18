965d61ca64eda1c4d377fca89de31a6ecc455bdb
happiRNG <happirng@gmail.com>
2022-03-18 15:54:35 +0500

    message

--- pages/main/hostel/hostel.php
+++ pages/main/hostel/hostel.php
@@ -1,5 +1,5 @@
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/templates/page/head.php' ?>
-    <link rel="stylesheet" href="world_skills.css">
+    <link rel="stylesheet" href="hostel.css">
     <title>Главная</title>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/templates/page/body.php' ?>
     <div class="card">
